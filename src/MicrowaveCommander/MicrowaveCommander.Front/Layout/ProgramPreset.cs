3324ad6029855c13a125d14fb1102b587a838535
Vinicius <costavinicius95@gmail.com>
2026-08-11 01:28:17 -0300

    feat: create preset programs

---
+++ src/MicrowaveCommander/MicrowaveCommander.Front/Layout/ProgramPreset.cs
@@ -0,0 +1,12 @@
+namespace MicrowaveCommander.Front.Layout;
+
+public class ProgramPreset
+{
+    public string Name { get; set; }
+    public string Meal { get; set; }
+    public int TimeInMinutes { get; set; }
+    public int TimeInSeconds { get; set; }
+    public int Power { get; set; }
+    public string Instructions { get; set; }
+    public string ProcessingString { get; set; }
+}
